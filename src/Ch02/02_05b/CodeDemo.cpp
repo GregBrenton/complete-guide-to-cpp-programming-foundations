6d3b93bcfc7e2c7a041c66e607629293a4bc7eb6
GregBrenton <gbrenton@jhacorp.com>
2025-05-01 03:49:53 +0000

    Finished video lesson

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,7 +4,22 @@
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
+    std::cout << "flag = " << my_flag << "\n";
+    my_flag = true;
+    std::cout << "flag = " << my_flag << "\n";
+    std::cout << "a + b = " << a + b << "\n";
+    std::cout << "b - a = " << b - a << "\n";
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << "\n";
     
     std::cout << std::endl << std::endl;
     return 0;
